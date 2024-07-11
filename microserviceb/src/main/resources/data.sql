7ecf77ec7c1c8214ec154a0bd060f2d43856d4c0
abilaunken <lucaxpto@gmail.com>
2024-07-11 15:04:24 -0300

    Delete microserviceb directory

--- microserviceb/src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO Country (name) VALUES ('India');
-INSERT INTO Country (name) VALUES ('Brazil');
-INSERT INTO Country (name) VALUES ('USA');
-INSERT INTO Country (name) VALUES ('Italy');
--- microserviceb/src/main/resources/schema.sql
+++
@@ -1,6 +0,0 @@
-drop table country;
-create table country(
-  ID bigint not null AUTO_INCREMENT,
-  name varchar(100) not null,
-  PRIMARY KEY ( ID )
-);
